50aa5d94fc23f9ddfa246f40b69b348f9d015466
collinsrobert <52458276+collinsrobert@users.noreply.github.com>
2024-09-24 12:37:46 -0500

    Create hideSQLInstance.ps1

---
+++ hideSQLInstance.ps1
@@ -0,0 +1,6 @@
+Set-ItemProperty HKLM:SOFTWARE\\Microsoft\\"Microsoft SQL Server"\\MSSQL16.MSSQLSERVER\\MSSQLServer\\SuperSocketNetLib -Name HideInstance -Value 1  ## This is for SQL Server 2022
+Set-ItemProperty HKLM:SOFTWARE\\Microsoft\\"Microsoft SQL Server"\\MSSQL15.MSSQLSERVER\\MSSQLServer\\SuperSocketNetLib -Name HideInstance -Value 1  ## This is for SQL Server 2019
+Set-ItemProperty HKLM:SOFTWARE\\Microsoft\\"Microsoft SQL Server"\\MSSQL14.MSSQLSERVER\\MSSQLServer\\SuperSocketNetLib -Name HideInstance -Value 1  ## This is for SQL Server 2017
+Set-ItemProperty HKLM:SOFTWARE\\Microsoft\\"Microsoft SQL Server"\\MSSQL13.MSSQLSERVER\\MSSQLServer\\SuperSocketNetLib -Name HideInstance -Value 1  ## This is for SQL Server 2016
+Set-ItemProperty HKLM:SOFTWARE\\Microsoft\\"Microsoft SQL Server"\\MSSQL12.MSSQLSERVER\\MSSQLServer\\SuperSocketNetLib -Name HideInstance -Value 1  ## This is for SQL Server 2014
+Set-ItemProperty HKLM:SOFTWARE\\Microsoft\\"Microsoft SQL Server"\\MSSQL11.MSSQLSERVER\\MSSQLServer\\SuperSocketNetLib -Name HideInstance -Value 1  ## This is for SQL Server 2012
