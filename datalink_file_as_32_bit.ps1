dc7744a749bacf422ad79f0e797a2643e57d0e21
Collins Robert <collins.o.robert@gmail.com>
2026-07-30 21:42:19 -0500

    Create datalink_file_as_32_bit.ps1

---
+++ datalink_file_as_32_bit.ps1
@@ -0,0 +1,3 @@
+##  create test.txt file, change the extension to .udl file
+## Call it below as a 32 bit version of data link file for testing 32 bit ODBC DSN created
+##C:\Windows\syswow64\rundll32.exe "C:\Program Files (x86)\Common Files\System\Ole DB\oledb32.dll",OpenDSLFile C:\E_\client32\ODAC1927_x86\test.udl
